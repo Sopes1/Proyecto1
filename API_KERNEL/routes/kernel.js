ca9daaed468155da2c310f53683ef30481010b69
JuanPa09 <juanpablo.ardon99@gmail.com>
2021-09-18 14:17:57 -0600

    Se agrego segunda peticion

--- API_KERNEL/routes/kernel.js
+++ API_KERNEL/routes/kernel.js
@@ -5,7 +5,17 @@ const ruta_modulo_ram = '/proc/rammodule';
 const ruta_modulo_cpu = '/proc/cpumodule';
 
 router.get('/getcpu',async function(req,res,next){
-    let data = fs.readFileSync(ruta_modulo_cpu,'utf8');
-    console.log(data)
-    res.send(data)
+    let dataResponse = fs.readFileSync(ruta_modulo_cpu,'utf8');
+    let data = dataResponse.split('\n')
+    console.log(dataResponse)
+    res.send({procesos:data[0],cpu:data[1]})
 })
+
+module.get('/getram',async function(req,res,next){
+    let dataResponse = fs.readFileSync(ruta_modulo_ram,'utf8');
+    let data = dataResponse.split('\n')
+    console.log(dataResponse)
+    res.send({MemoriaTotal: data[0],MemoriaUso:data[1],MemoriaLibre:data[2],Cached:data[3],Buffers:data[4],PorcentajeUso:data[5]})
+})
+
+module.exports = router;
