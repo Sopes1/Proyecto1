bec3236a1dd452341ad30e7cddc91e665544b6ed
JuanPa09 <juanpablo.ardon99@gmail.com>
2021-09-21 01:56:07 -0600

    Se quito parte de prometheus

--- API_KERNEL/index.js
+++ API_KERNEL/index.js
@@ -3,22 +3,6 @@ const app = express();
 var morgan = require('morgan');
 var cors = require('cors')
 const dotenv = require('dotenv').config()
-//Para Prometheus
-const client = require('prom-client')
-const register = new client.Registry()
-
-register.setDefaultLabels({
-    app: process.env.nombrePC
-})
-
-client.collectDefaultMetrics({register})
-
-app.get('/metrics' , async(req , res)=>{
-
-    res.setHeader('Content-Type',register.contentType)
-    res.end(await register.metrics())
-
-})
 
 const port = 3100;
 
