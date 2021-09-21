7beb6cfa81d8546702d324b09770cda969b20cc5
JuanPa09 <juanpablo.ardon99@gmail.com>
2021-09-20 21:03:53 -0600

    Se agrego prometheus

--- API_KERNEL/index.js
+++ API_KERNEL/index.js
@@ -2,6 +2,23 @@ const express = require('express');
 const app = express();
 var morgan = require('morgan');
 var cors = require('cors')
+const dotenv = require('dotenv').config()
+//Para Prometheus
+const client = require('prom-client')
+const register = new client.Registry()
+
+register.setDefaultLabels({
+    app: process.env.nombrePC
+})
+
+client.collectDefaultMetrics({register})
+
+app.get('/metrics' , (req , res)=>{
+
+    res.setHeader('Content-Type',register.contentType)
+    res.end(register.metrics())
+
+})
 
 const port = 3100;
 
